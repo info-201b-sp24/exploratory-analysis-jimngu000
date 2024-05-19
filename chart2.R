3704e2c64f129d0a6606c66a56d80507e2cbb7b6
Jimmy Nguyen <jimmydn@uw.edu>
2024-05-19 13:12:40 -0700

    Finished chart 3, fixed index.rmd

--- chart2.R
+++ chart2.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 
 chart2_data <- read.csv("C:/Users/wildm/info201/datasets/socialmedia/smmh.csv") 
-long_title = "Relationship between age and tendency to compare onself to others via social media (2023)"
+long_title = "Relationship Between Age And Tendency To Compare Onself To Others Via Social Media (2023)"
 plot <- ggplot(social_media_mental, aes(x = chart2_data$X1..What.is.your.age., 
                                         y = chart2_data$X15..On.a.scale.of.1.5..how.often.do.you.compare.yourself.to.other.successful.people.through.the.use.of.social.media.)) +
   geom_point() +
--- chart3.R
+++ chart3.R
@@ -3,15 +3,22 @@ library(tidyverse)
 library(dplyr)
 
 chart3_data <- read.csv("C:/Users/wildm/info201/datasets/socialmedia/smmh.csv")
-
-long_title = "Relationship between age and tendency to compare onself to others via social media (2023)"
+chart3_data <- chart3_data %>%
+  group_by(X4..Occupation.Status) %>%
+  summarise(
+    total = n(),
+    at_most_2_hrs = sum(X8..What.is.the.average.time.you.spend.on.social.media.every.day. == "Between 1 and 2 hours" |
+                        X8..What.is.the.average.time.you.spend.on.social.media.every.day. == "Less than an Hour"),
+    ratio = (at_most_2_hrs / total) * 100
+  )
+long_title = "% Using Less Than Or Equal To 2 Hours Of Social Media Per Day, By Occupation (2023)"
 plot <- ggplot(chart3_data, aes(x = chart3_data$X4..Occupation.Status,
-                                y = chart3_data$X8..What.is.the.average.time.you.spend.on.social.media.every.day)) +
+                                y = chart3_data$ratio)) +
   geom_bar(stat = 'identity') +
   labs(
     title = paste(strwrap(long_title, width = 50), collapse = "\n"),
-    x = "Age",
-    y = "Frequency of comparing oneself to others on social media"
+    x = "Occupation",
+    y = "% using at most 2 hours of social media daily"
   )
 
 print(plot)
